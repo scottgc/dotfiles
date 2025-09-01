0fe1b92e3a65b76728e3e568e5cf1e3039b80236
Scott Gibson-Craig <scott.gc@gmail.com>
2025-09-01 14:13:38 -0600

    feat: add oh-my-zsh and p10k powershell to application

--- zshrc.sh
+++ zshrc.sh
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # always source zprofile regardless of whether this is/isn't a login shell
 source ~/.zprofile
 
@@ -6,6 +13,12 @@ source ~/.shrc
 
 # History file
 export HISTFILE=~/.zsh_history
+
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
 
 # Don't show duplicate history entires
 setopt hist_find_no_dups
@@ -41,3 +54,6 @@ ZSH_AUTOSUGGESTIONS="$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggesti
 
 # to avoid non-zero exit code
 true
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
